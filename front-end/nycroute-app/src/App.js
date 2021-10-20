ce4982139d4c2658f7a96748baba28913e208b54
kairuihuang <6kairuih@gmail.com>
2021-10-20 03:39:56 -0400

    set up routing for multiple separate pages

--- front-end/nycroute-app/src/App.js
+++ front-end/nycroute-app/src/App.js
@@ -1,57 +1,50 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 
 import logo from "./logo.svg";
 import "./App.css";
 
-// this is the top-level component of the app, high-level components of our app
-// are added the DOM by inserting them into the JSX here
+// import your component functions for use in route links
+import { Test1 } from "./Test1.js";
+import { Test2 } from "./Test2.js";
+
 function App() {
+  // add links to your pages for now
   return (
-    <Router>
-      {/* A <Switch> looks through its children <Route>s and
-      renders the first one that matches the current URL. */}
-      <Switch>
-        <Route path="/test1">
-          <Test1 />
-        </Route>
-        <Route path="/test2">
-          <Test2 />
-        </Route>
-      </Switch>
-      <div className="App">
-        <header className="App-header">
-          <Link className="App-link" to="/test1">
-            Router Test 1
-          </Link>
-          <Link className="App-link" to="/test2">
-            Router Test 2
-          </Link>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello World!</p>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </Router>
+    <Switch>
+      <Route exact path="/" component={CreateReactHome}></Route>
+      <Route exact path="/test1" component={Test1}></Route>
+      <Route exact path="/test2" component={Test2}></Route>
+    </Switch>
   );
 }
 
-function Test1() {
-  return <h2>Test1</h2>;
-}
-
-function Test2() {
-  return <h2>Test2</h2>;
+function CreateReactHome() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello World!</p>
+        <Link className="App-link" to="/test1">
+          Test 1
+        </Link>
+        <Link className="App-link" to="/test2">
+          Test 2
+        </Link>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
---
+++ front-end/nycroute-app/src/Test1.js
@@ -0,0 +1,3 @@
+export function Test1() {
+  return <h2>Test 1</h2>;
+}
