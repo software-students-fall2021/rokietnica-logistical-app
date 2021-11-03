135335c4b067cdc8f00c4edbb1fe1396d626f8bc
kairuihuang <6kairuih@gmail.com>
2021-11-02 23:30:10 -0400

    add and move mock data to separate file

--- front-end/nycroute-app/src/SubwayStations.js
+++ front-end/nycroute-app/src/SubwayStations.js
@@ -1,29 +1,7 @@
 import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 
-// temp mock data
-const stationData = [
-  {
-    name: "8 St - NYU",
-    lines: ["R", "W"],
-  },
-  {
-    name: "West 4 St - Washington Square",
-    lines: ["A", "C"],
-  },
-  {
-    name: "14 St - Union Square",
-    lines: ["R", "W", "L", "6"],
-  },
-  {
-    name: "Astor Pl",
-    lines: ["4", "5", "6"],
-  },
-  {
-    name: "Times Square - 42 St",
-    lines: ["A", "C", "E", "N"], // a lot more in reality
-  },
-];
+import stationData from "./stationData";
 
 const SubwayStations = () => {
   return (
---
+++ front-end/nycroute-app/src/stationData.js
@@ -0,0 +1,101 @@
+// temp mock data
+const stationData = [
+  {
+    name: "8 St - NYU",
+    lines: ["R", "W"],
+  },
+  {
+    name: "West 4 St - Washington Square",
+    lines: ["A", "C"],
+  },
+  {
+    name: "14 St - Union Square",
+    lines: ["R", "W", "L", "6"],
+  },
+  {
+    name: "Astor Pl",
+    lines: ["4", "5", "6"],
+  },
+  {
+    name: "Times Square - 42 St",
+    lines: ["A", "C", "E", "N"], // a lot more in reality
+  },
+  {
+    name: "Lorem Ispum Dolor Sit Amet",
+    lines: ["A", "B", "C", "1", "2", "3"],
+  },
+  {
+    name: "Lorem Ispum Dolor Sit Amet",
+    lines: ["A", "B", "C", "1", "2", "3"],
+  },
+  {
+    name: "Lorem Ispum Dolor Sit Amet",
+    lines: ["A", "B", "C", "1", "2", "3"],
+  },
+  {
+    name: "Lorem Ispum Dolor Sit Amet",
+    lines: ["A", "B", "C", "1", "2", "3"],
+  },
+  {
+    name: "Lorem Ispum Dolor Sit Amet",
+    lines: ["A", "B", "C", "1", "2", "3"],
+  },
+  {
+    name: "Lorem Ispum Dolor Sit Amet",
+    lines: ["A", "B", "C", "1", "2", "3"],
+  },
+  {
+    name: "Lorem Ispum Dolor Sit Amet",
+    lines: ["A", "B", "C", "1", "2", "3"],
+  },
+  {
+    name: "Lorem Ispum Dolor Sit Amet",
+    lines: ["A", "B", "C", "1", "2", "3"],
+  },
+  {
+    name: "Lorem Ispum Dolor Sit Amet",
+    lines: ["A", "B", "C", "1", "2", "3"],
+  },
+  {
+    name: "Lorem Ispum Dolor Sit Amet",
+    lines: ["A", "B", "C", "1", "2", "3"],
+  },
+  {
+    name: "Lorem Ispum Dolor Sit Amet",
+    lines: ["A", "B", "C", "1", "2", "3"],
+  },
+  {
+    name: "Lorem Ispum Dolor Sit Amet",
+    lines: ["A", "B", "C", "1", "2", "3"],
+  },
+  {
+    name: "Lorem Ispum Dolor Sit Amet",
+    lines: ["A", "B", "C", "1", "2", "3"],
+  },
+  {
+    name: "Lorem Ispum Dolor Sit Amet",
+    lines: ["A", "B", "C", "1", "2", "3"],
+  },
+  {
+    name: "Lorem Ispum Dolor Sit Amet",
+    lines: ["A", "B", "C", "1", "2", "3"],
+  },
+  {
+    name: "Lorem Ispum Dolor Sit Amet",
+    lines: ["A", "B", "C", "1", "2", "3"],
+  },
+  {
+    name: "Lorem Ispum Dolor Sit Amet",
+    lines: ["A", "B", "C", "1", "2", "3"],
+  },
+  {
+    name: "Lorem Ispum Dolor Sit Amet",
+    lines: ["A", "B", "C", "1", "2", "3"],
+  },
+  {
+    name: "Lorem Ispum Dolor Sit Amet",
+    lines: ["A", "B", "C", "1", "2", "3"],
+  },
+];
+
+export default stationData;
