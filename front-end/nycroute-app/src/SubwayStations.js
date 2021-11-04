903fbc9ce25109be1af3d5a61c9cbe10b7167313
kairuihuang <6kairuih@gmail.com>
2021-11-03 21:03:10 -0400

    remove extra div

--- front-end/nycroute-app/src/SubwayStations.js
+++ front-end/nycroute-app/src/SubwayStations.js
@@ -6,20 +6,18 @@ import stationData from "./stationData";
 
 const SubwayStations = () => {
   return (
-    <div class="stationsWrapper">
-      <ListGroup>
-        {stationData.map((station) => (
-          <ListGroup.Item
-            action
-            onClick={() => {
-              alert(`tapped on ${station.name}`);
-            }}
-          >
-            {station.name}
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
-    </div>
+    <ListGroup className="stationsWrapper">
+      {stationData.map((station) => (
+        <ListGroup.Item
+          action
+          onClick={() => {
+            alert(`tapped on ${station.name}`);
+          }}
+        >
+          {station.name}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
   );
 };
 
