0afc92dc213fc43f9ca3a4c2b0d27a6b7d01b61e
Soojin Choi <ycsoojin@gmail.com>
2021-11-05 18:55:19 -0400

    backend init and install express, nodemon

---
+++ back-end/app.js
@@ -0,0 +1,14 @@
+// import and instantiate express
+const express = require("express") // CommonJS import style!
+const app = express() // instantiate an Express object
+
+// we will put some server logic here later...
+
+// export the express app we created to make it available to other modules
+
+// route for HTTP GET requests to the root document
+app.get("/", (req, res) => {
+    res.send("Goodbye world!")
+})
+
+module.exports = app
