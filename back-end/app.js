0016724cd63a649d419972c2a11e329bc070692e
Soojin Choi <ycsoojin@gmail.com>
2021-11-05 18:59:21 -0400

    install axios and test api call

--- back-end/app.js
+++ back-end/app.js
@@ -1,14 +1,22 @@
 // import and instantiate express
 const express = require("express") // CommonJS import style!
+
+const axios = require("axios") // middleware for making requests to APIs
+
 const app = express() // instantiate an Express object
-
-// we will put some server logic here later...
-
-// export the express app we created to make it available to other modules
 
 // route for HTTP GET requests to the root document
 app.get("/", (req, res) => {
     res.send("Goodbye world!")
 })
+
+// proxy requests to/from an API
+app.get("/apiCallTest", (req, res, next) => {
+    axios
+      .get("https://my.api.mockaroo.com/line0.json?key=57b58bf0")
+      .then(apiResponse => res.json(apiResponse.data)) // pass data along directly to client
+      .catch(err => next(err)) // pass any errors to express
+})
+
 
 module.exports = app
