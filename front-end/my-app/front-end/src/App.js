f32146c320b04f3d2b3f512f401dc140ce2988bd
Sanjana Dhindsa <56938647+sanjanadhindsa@users.noreply.github.com>
2021-11-09 20:35:46 -0500

    -m Committing changes

---
+++ front-end/my-app/front-end/src/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div
+      style={{
+        backgroundColor: 'pink',
+        width: '100px',
+        heigth: '100px'
+        
+      }}
+    />
+  );
+}
+
+export default App;
