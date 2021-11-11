aa06dfc389efa7a2f600d7ef7e58ce0b67618665
kairuihuang <6kairuih@gmail.com>
2021-11-11 16:43:23 -0500

    add borough data for stationData

--- front-end/nycroute-app/src/stationData.js
+++ front-end/nycroute-app/src/stationData.js
@@ -4,36 +4,43 @@ const stationData = [
     id: 1,
     name: "8 St - NYU",
     lines: ["R", "W"],
+    borough: "Manhattan",
   },
   {
     id: 2,
     name: "West 4 St - Washington Square",
     lines: ["A", "C"],
+    borough: "Manhattan",
   },
   {
     id: 3,
     name: "14 St - Union Square",
     lines: ["R", "W", "L", "6"],
+    borough: "Manhattan",
   },
   {
     id: 4,
     name: "Astor Pl",
     lines: ["4", "5", "6"],
+    borough: "Manhattan",
   },
   {
     id: 5,
     name: "Times Square - 42 St",
     lines: ["A", "C", "E", "N"], // a lot more in reality
+    borough: "Manhattan",
   },
   {
     id: 6,
     name: "Lorem Ispum Dolor Sit Amet",
     lines: ["A", "B", "C", "1", "2", "3"],
+    borough: "Staten Island",
   },
   {
     id: 7,
     name: "The Final Destination",
     lines: ["A", "B", "C", "1", "2", "3"],
+    borough: "Staten Island",
   },
 ];
 
