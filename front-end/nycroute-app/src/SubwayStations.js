8adb4b92a8ff96a70a58140aabc54f7982307f12
kairuihuang <6kairuih@gmail.com>
2021-11-11 16:36:21 -0500

    add dropdown component for borough filter

--- front-end/nycroute-app/src/SubwayStations.js
+++ front-end/nycroute-app/src/SubwayStations.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
+import Dropdown from "react-bootstrap/Dropdown";
 import { Link } from "react-router-dom";
 
 import "./SubwayStations.css";
@@ -9,6 +10,19 @@ const SubwayStations = () => {
   return (
     <div>
       <h1>Stations</h1>
+      <Dropdown className="filter">
+        <Dropdown.Toggle variant="info" id="dropdown-basic">
+          Filter by Bourough
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item>Manhattan</Dropdown.Item>
+          <Dropdown.Item>Brooklyn</Dropdown.Item>
+          <Dropdown.Item>Queens</Dropdown.Item>
+          <Dropdown.Item>Bronx</Dropdown.Item>
+          <Dropdown.Item>Staten Island</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
       <ListGroup className="stationsWrapper">
         {stationData.map((station) => (
           <Link className="App-link" to={`/stations/${station.id}`}>
