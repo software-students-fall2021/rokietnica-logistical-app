2819748af6492481e2c7f37c8b43fa4247ff1430
kairuihuang <6kairuih@gmail.com>
2021-11-11 16:15:24 -0500

    add page title

--- front-end/nycroute-app/src/SubwayStations.js
+++ front-end/nycroute-app/src/SubwayStations.js
@@ -7,13 +7,16 @@ import stationData from "./stationData";
 
 const SubwayStations = () => {
   return (
-    <ListGroup className="stationsWrapper">
-      {stationData.map((station) => (
-        <Link className="App-link" to={`/stations/${station.id}`}>
-          <ListGroup.Item action>{station.name}</ListGroup.Item>
-        </Link>
-      ))}
-    </ListGroup>
+    <div>
+      <h1>Stations</h1>
+      <ListGroup className="stationsWrapper">
+        {stationData.map((station) => (
+          <Link className="App-link" to={`/stations/${station.id}`}>
+            <ListGroup.Item action>{station.name}</ListGroup.Item>
+          </Link>
+        ))}
+      </ListGroup>
+    </div>
   );
 };
 
