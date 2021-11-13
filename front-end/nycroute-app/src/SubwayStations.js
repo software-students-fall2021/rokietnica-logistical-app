4cfeaa2824dba6a418aae49eb81123305c9b1d0b
kairuihuang <6kairuih@gmail.com>
2021-11-13 18:38:58 -0500

    remove filters (for now)

--- front-end/nycroute-app/src/SubwayStations.js
+++ front-end/nycroute-app/src/SubwayStations.js
@@ -10,19 +10,6 @@ const SubwayStations = () => {
   return (
     <div>
       <h1>Stations</h1>
-      <Dropdown className="filter">
-        <Dropdown.Toggle variant="info" id="dropdown-basic">
-          Filter by Borough
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu>
-          <Dropdown.Item>Manhattan</Dropdown.Item>
-          <Dropdown.Item>Brooklyn</Dropdown.Item>
-          <Dropdown.Item>Queens</Dropdown.Item>
-          <Dropdown.Item>Bronx</Dropdown.Item>
-          <Dropdown.Item>Staten Island</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
       <ListGroup className="stationsWrapper">
         {stationData.map((station) => (
           <Link className="App-link" to={`/stations/${station.id}`}>
