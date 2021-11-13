a7799c6161e4ea77bc7cf660e236ac527a8f50a1
kairuihuang <6kairuih@gmail.com>
2021-11-13 18:43:58 -0500

    sort stations by name

--- front-end/nycroute-app/src/SubwayStations.js
+++ front-end/nycroute-app/src/SubwayStations.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
-import Dropdown from "react-bootstrap/Dropdown";
 import { Link } from "react-router-dom";
 
 import "./SubwayStations.css";
 import stationData from "./stationData";
 
 const SubwayStations = () => {
+  stationData.sort((a, b) => (a.name >= b.name ? 1 : -1));
   return (
     <div>
       <h1>Stations</h1>
