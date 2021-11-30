f563b5ecc0fa4256b09e9f1878ef4f493a629f6a
Sanjana Dhindsa <56938647+sanjanadhindsa@users.noreply.github.com>
2021-11-29 22:40:13 -0500

    added mongoose code for connecting to db

--- back-end/server.js
+++ back-end/server.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+const { Mongoose } = require("mongoose");
 const server = require("./app"); // load up the web server
 const port = process.env.PORT || 4000; // the port to listen to for incoming requests
 // call express's listen function to start listening to the port
@@ -9,6 +10,11 @@ const listener = server.listen(port, function () {
 const close = () => {
   listener.close();
 };
+const db_url = process.env.MONGO_DB_URL;
+Mongoose.connect(db_url, () =>{
+  console.log("DB connection state: " + Mongoose.connection.readyState);
+});
+
 module.exports = {
   close: close,
 };
