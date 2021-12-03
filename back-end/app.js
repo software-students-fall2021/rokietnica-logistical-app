0353a5a2ad88ca6be2412da57cd043015eef1987
kairuihuang <6kairuih@gmail.com>
2021-12-02 20:43:30 -0500

    use mta api for the /station/:id route

--- back-end/app.js
+++ back-end/app.js
@@ -6,7 +6,6 @@ const parseCSV = require("./parseCSV");
 
 const fs = require("fs");
 
-
 const stationData = require("./stations");
 
 // allow CORS, so React app on port 3000 can make requests to Express server on port 4000
@@ -18,12 +17,14 @@ app.use((req, res, next) => {
   );
   next();
 });
-require("dotenv").config({ silent: true })
+
+require("dotenv").config({ silent: true });
 const mongoose = require("mongoose");
 const db_url = process.env.MONGO_DB_URL;
-mongoose.connect(db_url, () =>{
+mongoose.connect(db_url, () => {
   console.log("DB connection state: " + mongoose.connection.readyState);
 });
+
 // route for HTTP GET requests to the root document
 app.get("/", (req, res) => {
   res.send("Goodbye world!");
@@ -37,35 +38,89 @@ app.get("/apiCallTest", (req, res, next) => {
     .catch((err) => next(err)); // pass any errors to express
 });
 
-app.get("/stationData", (req, res) => {
-  
+app.get("/stationData", (req, res, next) => {
   axios
     .get("http://demo6882294.mockable.io/stations")
-    .then(response=>{
-      
-      
-    //stationData.parse(response.data);
-    stationData.columns = response.data[0];
-    stationData.stations = response.data;
-    for(var i  = 0; i < stationData.stations.length;i++){
-      var station = stationData.stations[i];
-      station["Daytime Routes"] = station["Daytime Routes"].toString().split(" ");
-    }
-    
-     res.json(stationData);
-     
-  })
-  .catch(error=>{
-    console.log(error);
-    res.send(error);
-  })
-})
+    .then((response) => {
+      //stationData.parse(response.data);
+      stationData.columns = response.data[0];
+      stationData.stations = response.data;
+      for (var i = 0; i < stationData.stations.length; i++) {
+        var station = stationData.stations[i];
+        station["Daytime Routes"] = station["Daytime Routes"]
+          .toString()
+          .split(" ");
+      }
 
-app.get("/station/:id", (req, res) => {
-  const station = stationData.stations.filter(
-    (st) => st["Station ID"] == req.params.id
-  );
-  res.json(station[0]);
+      res.json(stationData);
+    })
+    .catch((error) => {
+      console.log(error);
+      next(error);
+    });
 });
+
+app.get("/station/:id", (req, res, next) => {
+  let endpoint = "http://localhost:5000/by-id/" + req.params.id;
+  axios
+    .get(endpoint)
+    .then((response) => {
+      const data = parseStation(response.data);
+      console.log(data);
+      res.json(data);
+    })
+    .catch((error) => {
+      console.log(error);
+      next(error);
+    });
+});
+
+// utilities (move to another file later...)
+function parseStation(data) {
+  const dataObj = data.data[0];
+  const parsed = {};
+
+  parsed.id = dataObj.id;
+  parsed.name = dataObj.name;
+  parsed.updated = minutesAgo(data.updated);
+
+  // create skeleton objects for traintimes when iterating later
+  parsed.traintimes = {};
+  parsed.routes = dataObj.routes.sort();
+  parsed.routes.forEach((route) => {
+    parsed.traintimes[route] = {
+      uptown: [],
+      downtown: [],
+    };
+  });
+
+  for (const key of Object.keys(parsed.traintimes)) {
+    parsed.traintimes[key].uptown = getTrains(key, dataObj.N);
+    parsed.traintimes[key].downtown = getTrains(key, dataObj.S);
+  }
+
+  return parsed;
+}
+
+function minutesAway(datetimestr) {
+  const now = new Date();
+  const future = new Date(datetimestr);
+  const diff = future.getTime() - now.getTime();
+  return Math.floor(diff / 1000 / 60); // milliseconds to minutes, floored
+}
+
+function minutesAgo(datetimestr) {
+  const now = new Date();
+  const future = new Date(datetimestr);
+  const diff = now.getTime() - future.getTime();
+  return Math.round(diff / 1000 / 60); // milliseconds to minutes, floored
+}
+
+function getTrains(route, trains) {
+  const filtered = trains.filter((entry) => entry.route == route);
+  return filtered.map((entry) => {
+    return minutesAway(entry.time);
+  });
+}
 
 module.exports = app;
